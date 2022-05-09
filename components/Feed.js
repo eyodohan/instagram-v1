9be4f3e55411aa8d06377180b52691a458a5d5ed
Eyup Dogan <eyodohan@gmail.com>
2022-05-09 14:11:04 +0300

    populate fake data using minifaker and map through it inside story component

---
+++ components/Feed.js
@@ -0,0 +1,22 @@
+import Stories from './Stories';
+
+const Feed = () => {
+  return (
+    <main>
+      <section>
+        {/* stories */}
+        <Stories />
+
+        {/* posts */}
+      </section>
+
+      <section>
+        {/* mini profile */}
+
+        {/* suggestions */}
+      </section>
+    </main>
+  );
+};
+
+export default Feed;
---
+++ components/Story.js
@@ -0,0 +1,10 @@
+const Story = ({ img, username }) => {
+  return (
+    <div>
+      <img src={img} alt='username' />
+      <p>{username}</p>
+    </div>
+  );
+};
+
+export default Story;
